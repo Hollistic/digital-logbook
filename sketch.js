b8847b29b421af4a085a6c6cc23232681733d68d
Tar6269 <114016175+Tar6269@users.noreply.github.com>
2024-04-10 13:46:25 -0600

    freaky5.js
    we got a square now vro...

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER)
+  textAlign(CENTER)
+  rectMode(CENTER)
+}
+
+function lineUp(){
+  width = windowWidth
+  height = windowHeight
+}
+
+function windowResized(){
+  lineUp()
 }
 
 function draw() {
   background(220);
+  
+  square(width/2, height/2, sqrt(width*height)/2)
+
 }
