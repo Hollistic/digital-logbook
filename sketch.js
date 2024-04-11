af8dae3d59a77eec32303fb146ba653413f27541
Tar6269 <114016175+Tar6269@users.noreply.github.com>
2024-04-11 14:30:24 -0600

    papyrus
    font

--- sketch.js
+++ sketch.js
@@ -1,4 +1,9 @@
 let changered = false;
+// let ws = new WebSocket("");
+function preload() {
+papyrus = loadFont("assets/papyrus.ttf")
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER)
@@ -24,9 +29,9 @@ function draw() {
   text("hello vro", width/2, height*3.25/8);
   rect(width/2, height*5/8, width/2, height/4);
   text("hello bro", width/2, height*5.25/8);
-
-  text("")
-
+  textFont(papyrus);
+  textSize(150)
+  text("get freaky vro", width/2, height*5.25/8);
 
 }
 function mouseClicked(){
@@ -40,3 +45,17 @@ function mouseClicked(){
     print("hello bro");
   }
 } 
+
+function goOnline(){
+  
+}
+
+function sendData(Type, data) {
+  // sends data to the server; contains a message and a type, used to tell the server what to do with the received message data
+  let newMessage = { messageType: Type, data: data };
+  let newMessageString = JSON.stringify(newMessage);
+
+  ws.send(newMessageString);
+
+  print("message should be sent...");
+}
