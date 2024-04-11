a74be47d4749b37123b09b4f84edd0a7e3872b82
Tar6269 <114016175+Tar6269@users.noreply.github.com>
2024-04-11 14:18:16 -0600

    two very useful buttons added vro
    hello vro

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+let changered = false;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER)
@@ -13,10 +14,29 @@ function lineUp(){
 function windowResized(){
   lineUp()
 }
-
+function isBetween(lowerX, lowerY, upperX, upperY){
+  return mouseX > lowerX && mouseX < upperX && mouseY > lowerY && mouseY < upperY
+}
 function draw() {
   background(220);
-  
-  square(width/2, height/2, sqrt(width*height)/2)
+  textSize(50);
+  rect(width/2, height*3/8, width/2, height/4);
+  text("hello vro", width/2, height*3.25/8);
+  rect(width/2, height*5/8, width/2, height/4);
+  text("hello bro", width/2, height*5.25/8);
+
+  text("")
+
 
 }
+function mouseClicked(){
+  print('yo')
+  if (isBetween(width/4, height/4, width*3/4, height/2)){
+    changered = true;
+    print("hello vro");
+  }
+  if (isBetween(width/4, height/2, width*3/4, height*3/4)){
+    changered = true;
+    print("hello bro");
+  }
+} 
