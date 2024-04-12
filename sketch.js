b753ab3e9a94287089f1ec83b149bb816a361476
Tar6269 <114016175+Tar6269@users.noreply.github.com>
2024-04-12 01:01:55 -0600

    freaky ah buttons
    new freaky registration menu and WIP buttons

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 let changered = false;
+let mainMenu = true;
+let input;
+let registerMenu = false;
 // let ws = new WebSocket("");
 function preload() {
-papyrus = loadFont("assets/papyrus.ttf")
+// freaky = loadFont("assets/lastninja.ttf")
+freakyimg = loadImage("assets/freakyah.png")
+freakyfont = loadFont("assets/papyrus.ttf")
 }
 
 function setup() {
@@ -9,6 +14,7 @@ function setup() {
   imageMode(CENTER)
   textAlign(CENTER)
   rectMode(CENTER)
+  textFont(freakyfont)
 }
 
 function lineUp(){
@@ -25,29 +31,65 @@ function isBetween(lowerX, lowerY, upperX, upperY){
 function draw() {
   background(220);
   textSize(50);
-  rect(width/2, height*3/8, width/2, height/4);
-  text("hello vro", width/2, height*3.25/8);
-  rect(width/2, height*5/8, width/2, height/4);
-  text("hello bro", width/2, height*5.25/8);
-  textFont(papyrus);
-  textSize(150)
-  text("get freaky vro", width/2, height*5.25/8);
+  if (mainMenu){
+    drawMainMenu();
+  }
+  else if (registerMenu){
+    drawRegisterMenu();
+  }
+  // image(freakyimg, width/2, height/2)
 
 }
 function mouseClicked(){
   print('yo')
-  if (isBetween(width/4, height/4, width*3/4, height/2)){
-    changered = true;
-    print("hello vro");
-  }
-  if (isBetween(width/4, height/2, width*3/4, height*3/4)){
-    changered = true;
-    print("hello bro");
-  }
+  if (mainMenu){
+    if (isBetween(width/4, height/4, width*3/4, height/2)){
+      changered = true;
+      print("hello vro");
+      mainMenu = false;
+      registerMenu = true;
+      makeButton(width/2, height*3/8);
+
+    }
+    if (isBetween(width/4, height/2, width*3/4, height*3/4)){
+      changered = true;
+      print("hello bro");
+    }
+}
 } 
 
-function goOnline(){
-  
+function drawMainMenu(){
+  rect(width/2, height*3/8, width/2, height/4);
+  text("hello vro", width/2, height*3.25/8);
+  rect(width/2, height*5/8, width/2, height/4);
+  text("hello bro", width/2, height*5.25/8);
+  textSize(125);
+  text("get freaky vro", width/2, height/6.75);
+}
+function drawRegisterMenu(){
+
+}
+class Button{
+  constructor(x, y, txt){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.txt = txt;
+    this.button = createButton(txt)
+    this.button.position(x,y);
+    this.button.mousePressed(message())
+  }
+}
+function makeButton(buttonWidth, buttonHeight){
+  input = createInput();
+  input.position(buttonWidth, buttonHeight);
+  button = createButton("ok vro");
+  button.position(input.x + input.width*1.2, height/2);
+  button.mousePressed(message);
+
+}
+function message() {
+  print("hello" + input.value() +  "vro")
 }
 
 function sendData(Type, data) {
